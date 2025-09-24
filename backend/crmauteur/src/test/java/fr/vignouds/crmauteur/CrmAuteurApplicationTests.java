20f46b606710f780a4498e438697c554ccd50134
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2025-09-24 15:51:10 +0200

    infra: init spring boot project

---
+++ backend/crmauteur/src/test/java/fr/vignouds/crmauteur/CrmAuteurApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.vignouds.crmauteur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmAuteurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
